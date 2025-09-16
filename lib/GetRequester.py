233889fd64eeed756e6a29cfaaebe219987b3ec7
laurenannmurugi <laurenann1871@gmail.com>
2025-09-16 23:33:02 +0300

    Complete lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,14 @@
 import requests
-import json
+
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content  # returns the raw body
 
     def load_json(self):
-        pass
+        response = requests.get(self.url)
+        return response.json()  # returns Python list/dict from JSON
